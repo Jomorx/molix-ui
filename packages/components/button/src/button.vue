6bd66996df34b3c1d06dddcb0b0faef700755065
huchanghao <2955568032@qq.com>
2023-02-21 18:07:19 +0800

    🚧WIP(Jomorx): preliminary completion button round type

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+interface IButtonProps {
+    //按钮类型
+    type?: "primary" | "error" | "info" | "success";
+    disabled?: true | false;
+    round?: true | false;
+}
+interface IButtonEmits {
+    (e: "click", event: MouseEvent): any;
+}
+export default {
+    name: "ml-button",
+};
+</script>
+
+<script lang="ts" setup>
+const props = withDefaults(defineProps<IButtonProps>(), {
+    type: "primary",
+    disabled: false,
+    round: false,
+});
+const emit = defineEmits<IButtonEmits>();
+const handleClick = (e: MouseEvent) => {
+    emit("click", e);
+};
+</script>
+
+<template>
+    <button @click="handleClick" :class="{ btn: true, [type]: true, round }" :disabled="disabled">
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+@import "../../../theme/src/button.scss";
+</style>
