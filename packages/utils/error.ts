5f9a71d37e6f1f6fc7c7a3e1eb4d0bb1a7bae017
Linxae <702611663@qq.com>
2023-02-27 19:03:22 +0800

    🎀refactor(Linxae): change vitest config file to vite config

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -1,5 +1,5 @@
 import { isString } from "lodash";
-class MolixError extends Error {
+export class MolixError extends Error {
     constructor(m: string) {
         super(m);
         this.name = "MolixError";
@@ -13,7 +13,8 @@ export function throwError(scope: string, m: string): never {
 export function debugWarn(err: Error): void;
 export function debugWarn(scope: string, message: string): void;
 export function debugWarn(scope: string | Error, message?: string): void {
-    if (process.env.NODE_ENV !== "production") {
+    // @ts-ignore
+    if (process.env.NODE_ENV !== "production" && this === window) {
         const error: Error = isString(scope) ? new MolixError(`[${scope}] ${message}`) : scope;
         // eslint-disable-next-line no-console
         console.warn(error);
