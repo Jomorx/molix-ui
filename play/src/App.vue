96cd964c0bd33dae934df188f4024f6b7d207c46
Linxae <702611663@qq.com>
2023-02-27 12:46:18 +0800

    🚧WIP(Linxae): add rest css

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,11 +1,9 @@
 <template>
     <MlRate :max="10" v-model="rate" show-score></MlRate>
-    <MlInput v-model="input" placeholder="输入一下罢"></MlInput>
+    <MlInput placeholder="输入一下罢"></MlInput>
     <ElInput></ElInput>
-    <MlButton>啊啊</MlButton>
 </template>
 <script setup lang="ts">
-import { MlButton } from "@molix/components";
 import { ref } from "vue";
 const input = ref("");
 const rate = ref(0);
@@ -13,6 +11,6 @@ const rate = ref(0);
 
 <style scoped>
 .ml-input {
-    margin: 10px;
+    margin: 10px 0px;
 }
 </style>
